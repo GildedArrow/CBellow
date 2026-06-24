3440bb9933c3efe6c9272babc87a16028f569b1d
GildedArrow <162296582+GildedArrow@users.noreply.github.com>
2026-06-24 13:52:48 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -3,10 +3,16 @@
 #include <stdbool.h>
 
 typedef enum {
-	LEX_NUMBER, LEX_AMPERSAND, LEX_HASHTAG,
+	LEX_EOF, LEX_NUMBER, LEX_AMPERSAND, LEX_HASHTAG,
 	LEX_LABEL, LEX_LABELDEF, LEX_COMMA, LEX_NEWLINE,
 	LEX_PERIOD, LEX_INSTRUCTION
 } BLexTokenType;
+
+static const char *BReservedKeywords[] = {
+	"mov", "add", "sub", "div", "mul", "mod",
+	"prc", "jmp", "jnz", "jsr", "jz", "ret",
+	"inc", "dec", "cmp", "inp"
+};
 
 typedef struct {
 	BLexTokenType type;
@@ -15,12 +21,12 @@ typedef struct {
 		int value;
 		char character;
 		char *string;
-	};
+	} data;
 } BToken;
 
 typedef struct {
 	char *src;
-	int line, start;
+	int line, pos;
 	int end;
 	bool haderror;
 } BLexer;
@@ -73,7 +79,7 @@ BLexer *Create_Bellow_Lexer(const char *source) {
 	Load_Source(lexer, source);
 	
 	lexer->line = 1;
-	lexer->start = 0;
+	lexer->pos = 0;
 	lexer->haderror = false;
 	
 	return lexer;
@@ -84,13 +90,97 @@ void Free_Bellow_Lexer(BLexer *lexer) {
 	free(lexer);
 }
 
-int main(int argc, char *argv[]) {
+
+char advance(BLexer *lexer) {
+	return ++lexer->pos;
+}
+
+char peek(BLexer *lexer) {
+	return lexer->pos + 1;
+}
+
+bool isAlpha(char c) {
+	return (c > 64 && c < 91) ||
+	(c > 96 && c < 123) ||
+	(c == 95);
+}
+
+bool isNumeric(char c) {
+	return c > 47 && c < 58;
+}
+
+bool isAlphaNumeric(char c) {
+	return isAlpha(c) || isNumeric(c);
+}
+
+void skipwhitespace(BLexer *lexer) {
 	
+}
+
+void skipcomments(BLexer *lexer) {
+	
+}
+
+void instructionorlabel(BLexer *lexer) {
+	
+}
+
+void number(BLexer *lexer) {
+	
+}
+
+BToken MakeCharToken(BLexTokenType type, int line, char c) {
+	BToken t;
+	
+	t.type = type;
+	t.line = line;
+	t.data.character = c;
+	
+	return t;
+}
+
+BToken NextToken(BLexer *lexer) {
+	BToken t;
+	char c;
+	
+	while (c != '\0') {
+		c = advance(lexer);
+		
+		switch (c) {
+			case '\0':	
+				return MakeCharToken(LEX_EOF, lexer->line, '\0');
+			case ' ':
+				skipwhitespace(lexer);
+				break;
+			case ';':
+				skipcomments(lexer);
+				break;
+			case '\n':
+				return MakeCharToken(LEX_NEWLINE, lexer->line, '\n');
+			case '.':
+				return MakeCharToken(LEX_PERIOD, lexer->line, '.');
+			case ',':
+				return MakeCharToken(LEX_COMMA, lexer->line, ',');
+			case '&':
+				return MakeCharToken(LEX_AMPERSAND, lexer->line, '&');
+			case '#':
+				return MakeCharToken(LEX_HASHTAG, lexer->line, '#');			
+		}
+		
+		if (isAlpha(c)) {
+			instructionorlabel(lexer);
+		}
+		
+		if (isNumeric(c)) {
+			number(lexer);	
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
 	BLexer *lexer = Create_Bellow_Lexer("fibonacci.bellow");
 	
-	printf("%s", lexer->src);
-	
-	
+	NextToken(lexer);
 	
 	Free_Bellow_Lexer(lexer);
 	
